5d8038e9de1e84940545ea9624f0691c2aae6602
Wesley Willis <haileyw@gmail.com>
2015-11-29 15:51:52 -0600

    add code for monthly shift totals per worker

--- break_project/app/models/worker.rb
+++ break_project/app/models/worker.rb
@@ -1,4 +1,15 @@
 class Worker < ActiveRecord::Base
   belongs_to :client
   has_many :hours
+
+  def month_shifts
+    shifts.where("start_time >= ?", Time.now.beginning_of_month)
+  end
+
+  def month_totals
+    total = 0
+    month_shifts.each do shift
+      total += (shift.stop - shift.start)
+    end  
+  end
 end
