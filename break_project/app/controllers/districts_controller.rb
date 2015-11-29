bf94a42530d4f5edc6ba3fe643f07a0df60e2add
Wesley Willis <haileyw@gmail.com>
2015-11-28 19:45:50 -0600

    fix new district form

--- break_project/app/controllers/districts_controller.rb
+++ break_project/app/controllers/districts_controller.rb
@@ -7,4 +7,35 @@ class DistrictsController < ApplicationController
     id = params[:id]
     @district = District.find(id)
   end
+
+  def create
+    District.create(district_params)
+    redirect_to districts_path
+  end
+
+  def new
+    @district = District.new
+    @action = "create"
+    @method = :post
+    @title = "District Information"
+  end
+
+  def edit
+    @title = "Edit your district"
+    id = params[:id]
+    @action = "update"
+    @method = :patch
+    @district = District.find(id)
+    render "new"
+  end
+
+  def update
+    District.update(params[:id], district_params[:district])
+    redirect_to district_path(params[:id])
+  end
+
+  private
+  def district_params
+    params.require(:district).permit(:name, :address, :city, :state, :zip)
+  end
 end
