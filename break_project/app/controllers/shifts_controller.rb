3aca194f11210339acb31466f8ce5939153a14de
Wesley Willis <haileyw@gmail.com>
2015-11-29 18:40:39 -0600

    added styling, broke shift tracker

--- break_project/app/controllers/shifts_controller.rb
+++ break_project/app/controllers/shifts_controller.rb
@@ -3,20 +3,40 @@ class ShiftsController < ApplicationController
     @shift = Shift.new
     @worker = Worker.find(params[:worker_id])
     @client = Client.find(@worker.client_id)
+    @header = "Clock in?"
+    @action = "create"
+    @method = :post
+    @label = :start
   end
 
-#need to think about how this method will worke
-#maybe new will mean clock in
-#and edit will mean clock out
+  def edit
+    @header = "Clock out?"
+    id = params[:id]
+    @shift = Shift.find(id)
+    @action = "update"
+    @method = :patch
+    @label = :stop
+    render "new"
+  end
+
   def create
     worker = Worker.find(params[:worker_id])
     client = Client.find(worker.client_id)
 
     worker.shifts.create(
       start: Time.now.strftime('%H:%M:%S').to_i,
-      stop: Time.now.strftime('%H:%M:%S').to_i,
       worker_id: params[:worker_id],
       )
+    redirect_to district_client_worker_path(client.district_id, client.id, worker.id)
+  end
+
+  def update
+    worker = Worker.find(params[:worker_id])
+    client = Client.find(worker.client_id)
+    shift = worker.shifts.find(params[:id])
+    shift.update(
+    stop: Time.now.strftime('%H:%M:%S').to_i,
+    )
     redirect_to district_client_worker_path(client.district_id, client.id, worker.id)
   end
 
